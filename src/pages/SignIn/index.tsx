7959a2e9a11664e25ef718c5de7c727b1832253f
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-09-21 20:35:35 -0300

    chore(src): pages,routes, types

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import { Container, Title } from './styles';
+import Logo from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={Logo} />
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
+};
+export default SignIn;
