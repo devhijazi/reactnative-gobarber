bdcf7e617699e06e0505a66048a38595014526ce
Gabriel "Dev HijaZi" F <46979459+devhijazi@users.noreply.github.com>
2020-09-24 16:02:44 -0300

    chore(button): fixes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,13 +4,13 @@ import { RectButtonProperties } from 'react-native-gesture-handler';
 import { Container, ButtonText } from './styles';
 
 interface ButtonProps extends RectButtonProperties {
-    children: string;
+  children: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
-    <Container {...rest}>
-        <ButtonText>{children}</ButtonText>
-    </Container>
+  <Container {...rest}>
+    <ButtonText>{children}</ButtonText>
+  </Container>
 );
 
 export default Button;
